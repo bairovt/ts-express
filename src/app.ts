c0554dab1c0cdfc957f4ef3bae5a549849e10c18
Tumen Bairov <bairovt@gmail.com>
2019-07-01 20:04:58 +0900

    logging

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
-import express from 'express';
 import config from 'config';
+import express from 'express';
+const morgan = require('morgan');
 
 const serverPort = config.get('server.port')
 
 const app: express.Application = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Hello Peace!')
